b5833b018792138ae9e689b89e53b8836bcbf67f
Arosha De Silva <arosha@appletechlabs.com>
2018-03-12 13:03:22 +0530

    hm

--- src/Client.php
+++ src/Client.php
@@ -2,85 +2,95 @@
 
 namespace appletechlabs\flight;
 
+
 use appletechlabs\flight\Providers\AmadeusSoapProvider;
+class Client {
+  public $AmadeusSoap;
 
-class Client
-{
-    public $AmadeusSoap;
+  public function __construct($data = [])
+  {
 
-    public function __construct($data = [])
-    {
         $this->loadFromArray($data);
-    }
+  }
 
-    protected function loadFromArray(array $data)
-    {
+  protected function loadFromArray(array $data)
+  {
+
         if (count($data) > 0) {
             if (isset($data['amadeus'])) {
                 $this->AmadeusSoap = new AmadeusSoapProvider();
                 $this->AmadeusSoap->setup($data['amadeus']);
             }
         }
-    }
+  }
 
-    public function test()
-    {
+  public function test()
+  {
+
         return $this->AmadeusSoap;
-    }
+  }
 
-    public function signIn()
-    {
+  public function signIn()
+  {
+
         return $this->AmadeusSoap->securitySignIn();
-    }
+  }
 
-    public function signOut()
-    {
+  public function signOut()
+  {
+
         return $this->AmadeusSoap->securitySignOut();
-    }
+  }
 
-    public function FareMasterPricerCalendar($calendarSearchOpt)
-    {
+  public function FareMasterPricerCalendar($calendarSearchOpt)
+  {
+
         $calendarResult = $this->AmadeusSoap->FareMasterPricerCalendar($calendarSearchOpt);
 
         return $calendarResult;
-    }
+  }
 
-    public function FareMasterPricerCalendarSort($calendarResult)
-    {
+  public function FareMasterPricerCalendarSort($calendarResult)
+  {
+
         if ($calendarResult['result']->status == 'OK') {
             return $this->AmadeusSoap->calendarMin($calendarResult['result']);
         } else {
             return $calendarResult;
         }
-    }
+  }
 
-    public function FareMasterPricerTravelboardSearch($Opt)
-    {
+  public function FareMasterPricerTravelboardSearch($Opt)
+  {
+
         $fmptResult = $this->AmadeusSoap->FareMasterPricerTravelboardSearch($Opt);
 
         return $fmptResult;
-    }
+  }
 
-    public function fareBoardSearchOptimzed($fmptResult)
-    {
+  public function fareBoardSearchOptimzed($fmptResult)
+  {
+
         if ($fmptResult['result']->status == 'OK') {
             return  $this->AmadeusSoap->optimizeResults($fmptResult['result']);
         } else {
             return $fmptResult['result'];
         }
-    }
+  }
 
-    public function fareBoardSearchReturnOptimzed($fmptResult)
-    {
+  public function fareBoardSearchReturnOptimzed($fmptResult)
+  {
+
         if ($fmptResult['result']->status == 'OK') {
             return  $this->AmadeusSoap->optimizeResultsReturn($fmptResult['result']);
         } else {
             return $fmptResult['result'];
         }
-    }
+  }
 
-    public function fareBoardAndCalendarSearch($Opt, $calendarSearchOpt)
-    {
+  public function fareBoardAndCalendarSearch($Opt, $calendarSearchOpt)
+  {
+
         $calendarResult = $this->FareMasterPricerCalendar($calendarSearchOpt);
         $fmptResult = $this->FareMasterPricerTravelboardSearch($Opt);
         $result = [];
@@ -89,10 +99,11 @@ class Client
         $result['fareBoardSearch'] = $fmptResult;
 
         return $result;
-    }
+  }
 
-    public function fareBoardAndCalendarSearchOptimzed($Opt, $calendarSearchOpt, $type = 'oneway')
-    {
+  public function fareBoardAndCalendarSearchOptimzed($Opt, $calendarSearchOpt, $type = 'oneway')
+  {
+
         $rawResult = $this->fareBoardAndCalendarSearch($Opt, $calendarSearchOpt);
         $rawcalendarResult = $rawResult['calendarSearch'];
         $rawfmptResult = $rawResult['fareBoardSearch'];
@@ -126,72 +137,87 @@ class Client
         }
 
         return $result;
-    }
+  }
 
-    public function SellFromRecommendation($options)
-    {
+  public function SellFromRecommendation($options)
+  {
+
         $airSellRec = $this->AmadeusSoap->Air_SellFromRecommendation($options);
 
         return $airSellRec;
-    }
+  }
 
-    public function PNR_AddMultiElements($itinerary, $contactInfo)
-    {
+  public function PNR_AddMultiElements($itinerary, $contactInfo)
+  {
+
         return $this->AmadeusSoap->PNR_AddMultiElements($itinerary, $contactInfo);
-    }
+  }
 
-    public function PNR_Retrieve($pnr)
-    {
+  public function PNR_Retrieve($pnr)
+  {
+
         return $this->AmadeusSoap->PNR_Retrieve($pnr);
-    }
+  }
 
-    public function FarePricePnrWithBookingClass($validatingCarrier)
-    {
+  public function FarePricePnrWithBookingClass($validatingCarrier)
+  {
+
         return $this->AmadeusSoap->FarePricePnrWithBookingClassOptions($validatingCarrier);
-    }
+  }
 
-    public function createTSTFromPricing()
-    {
+  public function createTSTFromPricing()
+  {
+
         return $this->AmadeusSoap->createTSTFromPricing();
-    }
+  }
 
-    public function PNR_AddMultiElementsEnd()
-    {
+  public function PNR_AddMultiElementsEnd()
+  {
+
         return $this->AmadeusSoap->PNR_AddMultiElementsEnd();
-    }
+  }
 
-    public function docIssuance()
-    {
+  public function docIssuance()
+  {
+
         return $this->AmadeusSoap->docIssuance();
-    }
+  }
 
-    public function getAllPnr()
-    {
+  public function getAllPnr()
+  {
+
         return $this->AmadeusSoap->PNR_Retrieve_By_Office();
-    }
+  }
 
-    public function PNR_Cancel($pnr)
-    {
+  public function PNR_Cancel($pnr)
+  {
+
         return $this->AmadeusSoap->PNR_Cancel($pnr);
-    }
+  }
 
-    public function getLastRequest()
-    {
+  public function getLastRequest()
+  {
+
         return $this->AmadeusSoap->getLastRequest();
-    }
+  }
 
-    public function getLastResponse()
-    {
+  public function getLastResponse()
+  {
+
         return $this->AmadeusSoap->getLastResponse();
-    }
+  }
 
-    public function getLastRequestHeaders()
-    {
+  public function getLastRequestHeaders()
+  {
+
         return $this->AmadeusSoap->getLastRequestHeaders();
-    }
+  }
 
-    public function getLastResponseHeaders()
-    {
+  public function getLastResponseHeaders()
+  {
+
         return $this->AmadeusSoap->getLastResponseHeaders();
-    }
+  }
+
 }
+
